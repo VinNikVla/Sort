d67af3120450a1c843ba6741b2391d6ddb76c39c
VinNikVla <vinnik1905@yandex.ru>
2020-12-23 17:55:41 +0300

    change spindox to slider

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,32 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDesktopWidget>
+#include <QRect>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-    //QObject::connect(ui->timeItration, SIGNAL(valueChanged(int)), ui->pool, SLOT(setTimeIteration(int)));
-    QObject::connect(ui->buttonBegin, SIGNAL(pressed()), ui->pool, SLOT(slotNewGame()));
-    QObject::connect(ui->buttonClose, SIGNAL(clicked()), this, SLOT(close()));
-    QObject::connect(ui->numberDiscrets, SIGNAL(valueChanged(int)), ui->pool, SLOT(setNumberDiscrets(int)));
-    ui->numberDiscrets->setRange(2, 300);
-    ui->timeItration->setRange(1, 1000);
+    QDesktopWidget* p_desktop = QApplication::desktop();
+    const QRect sizeDesktop = p_desktop->screenGeometry();
 
+    ui->setupUi(this);
+    QObject::connect(ui->buttonBegin, SIGNAL(pressed()), ui->pool, SLOT(slotNewGame()));
     QObject::connect(ui->pool, SIGNAL(signalStartSort(bool)), ui->buttonBegin, SLOT(setDisabled(bool)));
-    QObject::connect(ui->pool, SIGNAL(signalStartSort(bool)), ui->numberDiscrets, SLOT(setDisabled(bool)));
-    //QObject::connect(ui->pool, SIGNAL(signalStartSort(bool)), ui->timeItration, SLOT(setDisabled(bool)));
-    QObject::connect(ui->pool, SIGNAL(signalStartSort(bool)), ui->typeSort, SLOT(setDisabled(bool)));
+
+    QObject::connect(ui->buttonClose, SIGNAL(clicked()), this, SLOT(close()));
+
+    QObject::connect(ui->sliderNumberDiscrets, SIGNAL(valueChanged(int)), ui->pool, SLOT(setNumberDiscrets(int)));
+    ui->sliderNumberDiscrets->setRange(2, 351);
+    //ui->sliderNumberDiscrets->s
+    QObject::connect(ui->sliderNumberDiscrets, SIGNAL(valueChanged(int)), ui->labelDisplayNemberDiscrets, SLOT(setNum(int)));
+    ui->labelDisplayNemberDiscrets->setText("2");
+    QObject::connect(ui->pool, SIGNAL(signalStartSort(bool)), ui->sliderNumberDiscrets, SLOT(setDisabled(bool)));
+    ui->labelNumberDiscrets->setAlignment(Qt::AlignCenter);
+    ui->labelNumberDiscrets->setText("Число отсчётов, шт");
 
     QObject::connect(ui->pool, SIGNAL(signalPause(bool)), ui->buttonPause, SLOT(setDisabled(bool)));
-
     QObject::connect(ui->buttonPause, SIGNAL(clicked()), ui->pool, SLOT(setPausa()));
-        ui->buttonPause->setCheckable(true);
-        ui->labelTimeIteration->setAlignment(Qt::AlignCenter);
-        ui->sliderTimeItreation->setTickPosition(QSlider::TicksBothSides);
-        ui->sliderTimeItreation->setTickInterval(10);
-        ui->sliderTimeItreation->setRange(1, 1000);
+    ui->buttonPause->setCheckable(true);
+
+    ui->labelTimeIteration->setAlignment(Qt::AlignCenter);
+    ui->sliderTimeItreation->setTickInterval(10);
+    ui->sliderTimeItreation->setRange(1, 1000);
     QObject::connect(ui->sliderTimeItreation, SIGNAL(valueChanged(int)), ui->pool, SLOT(setTimeIteration(int)));
+    ui->labelDisplayTimeIteration->setText("1");
+    QObject::connect(ui->sliderTimeItreation, SIGNAL(valueChanged(int)), ui->labelDisplayTimeIteration, SLOT(setNum(int)));
+    ui->buttonTypeSort->setText("Тип сортировки");
+    QMenu* pMenu = new QMenu(ui->buttonTypeSort);
+    pMenu->addAction("Пузырьковая");
+    pMenu->addAction("Быстрая");
+    pMenu->addAction("Херовая");
+
+    ui->buttonTypeSort->setMenu(pMenu);
+
+    ui->verticalLayout->setGeometry(sizeDesktop);
+
 
 
 }
@@ -38,12 +56,5 @@ MainWindow::~MainWindow()
 
 void MainWindow::changeNamePause(bool state)
 {
-    if(state)
-    {
-        ui->buttonPause->setText("Возобновить");
-    }
-    else
-    {
-        ui->buttonPause->setText("Пауза");
-    }
+    ui->buttonPause->setText(state ? "Возобновить" : "Пауза");
 }
